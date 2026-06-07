0b0e999a615fb9c128df1a590ed19d32e1ca2b63
jain678 <108728948+jain678@users.noreply.github.com>
2026-06-07 12:17:49 +0530

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if(beginWord == endWord || !wordList.contains(endWord)) return 0;
+
+        Set<String> st = new HashSet<>(wordList);
+        int n = wordList.size();
+        int m = wordList.get(0).length();
+
+        List<Integer>[] adj = new ArrayList[n];
+        for(int i= 0; i< n; i++){
+            adj[i]= new ArrayList<>();
+        }
+        int res = 0;
+        Queue<String> q = new LinkedList<>();
+        q.add(beginWord);
+        while(!q.isEmpty()){
+            res++;
+            for(int j=0; j< q.size(); j++){
+                String s = q.poll();
+                if( s == endWord) return res;
+                for(int i = 0 ; i< s.length(); i++){
+
+                    for(char c = 'a'; c <= 'z'; c++){
+                        if(c == s.charAt(i)) continue;
+                        String nei = s.substring(0, i) + c + s.substring(i+1);
+
+                        if(st.contains(nei)){
+                            q.add(nei);
+                            st.remove(nei);
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
